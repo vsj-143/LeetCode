a3b1593ee316c03521a64afeb32e1dc6d35cdbe0
Sandeep Javvaji <95626772+vsj-143@users.noreply.github.com>
2022-10-12 10:40:49 +0530

    Time: 1 ms (49.71%), Space: 41.5 MB (82.38%) - LeetHub

--- 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.java
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.java
@@ -19,18 +19,42 @@
 // traverse the right subtree
 class Solution {
     public List<Integer> preorderTraversal(TreeNode root) {
+                List <Integer> list=new ArrayList<Integer>();
+
+        if(root==null){
+            return list;
+        }
+        Stack<TreeNode>stack =new Stack<>();
+        stack.push(root);
+        while(!stack.isEmpty()){
+            TreeNode temp=stack.pop();
+            // System.out.print(temp.data+"");
+            list.add(temp.val);
+            if(temp.right!=null){
+                stack.push(temp.right);
+            }
+             if(temp.left!=null){
+                stack.push(temp.left);
+            }
+            
+        }
+        return list;
         
-        List <Integer> list=new ArrayList<Integer>();
-        dfs(root,list);
-        return list;}
-        
-        private void dfs(TreeNode node,List<Integer> list){
         
         
-        if(node==null){
-     return ;   }
-       list.add(node.val);
-            dfs(node.left,list);
-            dfs(node.right,list);
+        
+        
+        // List <Integer> list=new ArrayList<Integer>();
+//         dfs(root,list);
+//         return list;}
+        
+//         private void dfs(TreeNode node,List<Integer> list){
+        
+        
+//         if(node==null){
+//      return ;   }
+//        list.add(node.val);
+//             dfs(node.left,list);
+//             dfs(node.right,list);
     }
 }
